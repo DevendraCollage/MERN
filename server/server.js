ef63c0f9366bf636a2bd13fc005efeb79b9f3a40
Devendra Parmar <23030501043@darshan.ac.in>
2024-09-28 15:25:07 +0530

    Minor changes to Backend app and add the contact form details and login

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
-import errorMiddleware from "./Middlewares/error-middleware";
+const errorMiddleware = require("./Middlewares/error-middleware");
 
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = 5000;
-const router = require("./router/auth-router");
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
 const connectDB = require("./Utils/db");
 
 app.use(express.json()); // Middleware - This will parse the incoming request body
 
 // Mount the Router : To use the router in your main Express app, you can "mount" it at a specific URL prefix
-app.use("/api/auth", router);
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
 
 app.use(errorMiddleware);
 
