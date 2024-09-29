0c7f2fa94e208d724de82a10eb1ee32fb18a3179
Devendra Parmar <23030501043@darshan.ac.in>
2024-09-29 15:55:33 +0530

    Implement the Client side of the App and implement simple navigation using react-router through navigation

---
+++ client/src/Pages/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h1>Hello, from Register Page!</h1>
+    </div>
+  );
+};
+
+export default Register;
---
+++ client/src/Pages/Service.jsx
@@ -0,0 +1,9 @@
+const Service = () => {
+  return (
+    <div>
+      <h1>Hello, from Service Page!</h1>
+    </div>
+  );
+};
+
+export default Service;
