13a4ffdcaa39f8931da06a30f6d9bb1d6e705a0c
Devendra Parmar <23030501043@darshan.ac.in>
2024-10-01 13:23:13 +0530

    Added the assets for my full stack app and implement the UI of my app and handle the registration form in data to the MongoDB Server

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,8 +1,110 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  // Handle input change
+  const handleInputChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  // Handle the submission of the form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
-    <div>
-      <h1>Hello, from Register Page!</h1>
-    </div>
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container gri grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/register.png"
+                  alt="Trying to fill the registration page"
+                />
+              </div>
+              {/* Lets tackle registration form */}
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Registration Form</h1>
+                <br />
+
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="username">Username</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="Username"
+                      id="username"
+                      required
+                      autoComplete="off"
+                      value={user.username}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone">Phone</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      placeholder="Phone"
+                      id="phone"
+                      required
+                      autoComplete="off"
+                      value={user.phone}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
   );
 };
 
